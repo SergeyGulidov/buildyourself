81fbe9b5be135a01f915cb49f18f67b8aedbb307
Sergey Gulidov <S.Gulidov@gmail.com>
2013-03-16 14:40:37 +0200

    Fix heroku asset

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,6 @@ module BuildYourSelf
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.i18n.fallbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
