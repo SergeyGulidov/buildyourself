68f1b94f867ae39ec72a53639d5f1105d236940c
Sergey Gulidov <S.Gulidov@gmail.com>
2013-03-19 22:04:39 +0200

    Add top-bar style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
 			return 'hide'
 		end
 	end
+
+	  def full_title(page_title)
+	    base_title = "Build Yourself"
+	    if page_title.empty?                             
+	      base_title                                      
+	    else
+	      "#{base_title} | #{page_title}"                 
+	    end
+	  end
+
+
 end
