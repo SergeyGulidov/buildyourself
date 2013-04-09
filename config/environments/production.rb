8558e13d20aa24d95110088be13dfe1a30091fe7
Sergey Gulidov <S.Gulidov@gmail.com>
2013-04-09 21:49:42 +0300

    Add new relic config v1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,6 @@ BuildYourSelf::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'yoda.lv' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
