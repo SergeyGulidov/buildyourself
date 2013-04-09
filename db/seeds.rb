219d4048cbb676ce348dd2a7da4b2b2e7300eabd
Sergey Gulidov <S.Gulidov@gmail.com>
2013-04-09 21:01:35 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-user=User.create!(email:'s.gulidov@gmail.com',
+User.create!(email:'s.gulidov@gmail.com',
 	password: '459185468816', role: 1)
+
+Category.create!(category_name_lv:"Sports", category_name_ru:"Спорт", category_slug:"sport")
+Type.create!(type_name_lv:"Boks", type_name_ru:"Бокс", type_slug:"box")
+Interval.create!(interval_name_lv:"Pusaudžiem", interval_name_ru:"Подросткам", interval_slug:"juniors")
+City.create!(city_name_lv:"Rīga", city_name_ru:"Рига", city_slug:"riga")
+Country.create!(country_name_lv:"Latvija", country_name_ru:"Латвия", country_slug:"latvia")
