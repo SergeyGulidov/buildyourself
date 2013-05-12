f5bf7de38fd1a451e6e1f3458fb7eff6c0f942ba
Sergey Gulidov <S.Gulidov@gmail.com>
2013-05-12 13:33:14 +0300

    Language bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,12 @@ class ApplicationController < ActionController::Base
   private
 
   	def set_locale
-  		I18n.locale = params[:locale] if params[:locale].present?
+      if params[:locale].present? 
+  		  I18n.locale = params[:locale] 
+      else
+        I18n.locale = 'lv'
   		#current_user.locale
+      end
   	end
 
   	def default_url_options(options = {})
